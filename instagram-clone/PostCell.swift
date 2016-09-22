6d1a8a3ef1ee7627b9164e62505e0c9a57981953
Slavik <vjacheslav.horbach@gmail.com>
2016-09-22 18:06:45 +0200

    completely finished UI

---
+++ instagram-clone/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  instagram-clone
+//
+//  Created by Vyacheslav Horbach on 22/09/16.
+//  Copyright © 2016 Vjaceslav Horbac. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
